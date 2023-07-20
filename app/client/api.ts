df8287c98bef358a5671b0efea4359fe7384e23a
庄建国 <9340100@qq.com>
2023-07-20 16:15:48 +0000

    fix proxy

--- app/client/api.ts
+++ app/client/api.ts
@@ -123,7 +123,7 @@ export function getHeaders() {
   const accessStore = useAccessStore.getState();
   let headers: Record<string, string> = {
     "Content-Type": "application/json",
-    "x-requested-with": "XMLHttpRequest",
+    // "x-requested-with": "XMLHttpRequest",
   };
 
   const makeBearer = (token: string) => `Bearer ${token.trim()}`;
