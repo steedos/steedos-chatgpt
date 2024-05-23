19b8b686ef0e3b8b6b7c1a6aeac6449d5c3c582a
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2024-05-23 09:44:23 +0800

    fix build

--- app/store/config.ts
+++ app/store/config.ts
@@ -33,7 +33,7 @@ export const DEFAULT_CONFIG = {
   avatar: "1f603",
   fontSize: 14,
   theme: Theme.Auto as Theme,
-  tightBorder: !!config?.isApp || true,
+  tightBorder: true, //!!config?.isApp ,
   sendPreviewBubble: true,
   enableAutoGenerateTitle: true,
   sidebarWidth: DEFAULT_SIDEBAR_WIDTH,
